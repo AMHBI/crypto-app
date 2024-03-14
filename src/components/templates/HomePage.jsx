b07609d14fe7de1924fb43134fac86ef59a8ccc1
Ali Mohebi <alimhbi1@gmail.com>
2024-03-14 18:30:04 +0330

    add pagination

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react";
 import CoinsTable from "../modules/CoinsTable";
 import { getCoinList } from "../../services/cryptoAPIs";
+import Pagination from "../modules/Pagination";
 
 const HomePage = () => {
     const [coins,setCoins] = useState([])
     const [isLoading,setIsLoading] = useState(true)
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
+    
     const getData = async () => {
-      const res = await fetch(getCoinList())
+      setIsLoading(true)
+      const res = await fetch(getCoinList(page))
       const json = await res.json()
       setCoins(json)
       setIsLoading(false)
     }
     getData()
-  }, []);
+  }, [page]);
   console.log(coins);
   return <div>
+    <Pagination  page={page} setPage={setPage} />
     <CoinsTable  coins={coins} isLoading={isLoading}/>
   </div>;
 };
--- src/services/cryptoAPIs.js
+++ src/services/cryptoAPIs.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY =  "CG-wvyunkB8Fu6JiEuoQ6ifdc3Z"
 
-export const getCoinList = () => {
-    return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`
+export const getCoinList = (page) => {
+    return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${page}&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`
 }
